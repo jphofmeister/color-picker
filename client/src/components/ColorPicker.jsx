27b8f3c01025bc9772df6d53767208a0a58ceafd
Joseph Hofmeister <josephhofmeister@josephs-air.attlocal.net>
2023-12-30 11:10:52 -0500

    convert native html dragging to custom dragging

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -19,7 +19,6 @@ const StyledColorPicker = styled.div`
     left: calc(${props => props.$dragHandlePosition.x}px - 10px);
     background-color: transparent;
     border: 3px solid #fff;
-    display: ${props => props.$isDragging === true ? "none" : "block"};
     height: 20px;
     width: 20px;
     border-radius: 20px;
@@ -32,7 +31,7 @@ const ColorPicker = (props) => {
 
   // * Available props:
   // * Properties: hue
-  // * Functions: 
+  // * Functions: setSaturation, setLightness
 
   // let componentName = "ColorPicker";
 
@@ -43,13 +42,35 @@ const ColorPicker = (props) => {
   let setLightness = isEmpty(props.setLightness) === false ? props.setLightness : noFunctionAvailable;
 
   const colorPickerRef = useRef(null);
-  const dragItemRef = useRef(null);
 
   const [colorPickerClientRect, setColorPickerClientRect] = useState({});
   const [colorPickerOffset, setColorPickerOffset] = useState({ x: 0, y: 0 });
   const [dragHandlePosition, setDragHandlePosition] = useState({ x: 0, y: 0 });
 
   const [isDragging, setIsDragging] = useState(false);
+
+  const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 });
+
+
+  // * save page scroll position to state to update the color picker offset -- 12/30/2023 JH
+  useEffect(() => {
+
+    const onScroll = () => {
+
+      let newScrollPosition = {
+        x: document.documentElement.scrollLeft,
+        y: document.documentElement.scrollTop
+      };
+
+      setScrollPosition(newScrollPosition);
+
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+
+  }, []);
 
 
   // * get offsets and boundingClientRect for the color picker -- 12/30/2023 JH
@@ -62,22 +83,19 @@ const ColorPicker = (props) => {
       setColorPickerClientRect(boundingClientRect);
 
       let newOffset = {
-        x: boundingClientRect.x,
-        y: boundingClientRect.y
+        x: boundingClientRect.x + scrollPosition.x,
+        y: boundingClientRect.y + scrollPosition.y
       };
 
       setColorPickerOffset(newOffset);
 
     };
 
-  }, [colorPickerRef]);
+  }, [colorPickerRef, scrollPosition]);
 
 
   // * if color picker handle goes outside the color picker box, reset to the edge of the box -- 
   useEffect(() => {
-
-    // console.log("dragHandlePosition", dragHandlePosition);
-    // console.log("colorPickerClientRect", colorPickerClientRect);
 
     let dragTop = dragHandlePosition.y;
     let dragLeft = dragHandlePosition.x;
@@ -113,69 +131,57 @@ const ColorPicker = (props) => {
   }, [dragHandlePosition, colorPickerOffset, colorPickerClientRect]);
 
 
+  // pageX = saturation 0 - 100
+  // pageY = brightness (not lightness) 0 - 100
+
+  // https://stackoverflow.com/a/31851617
+  // function hsv_to_hsl(h, s, v) {
+  //   // both hsv and hsl values are in [0, 1]
+  //   var l = (2 - s) * v / 2;
+
+  //   if (l != 0) {
+  //     if (l == 1) {
+  //       s = 0;
+  //     } else if (l < 0.5) {
+  //       s = s * v / (l * 2);
+  //     } else {
+  //       s = s * v / (2 - l * 2);
+  //     }
+  //   }
+
+  //   return [h, s, l];
+  // }
+
+
+  // const handleDrop = (event) => {
+
+  //   // event.dataTransfer.setDragImage(dragItemRef.current, dragItemRef.current.getBoundingClientRect().width / 2, dragItemRef.current.getBoundingClientRect().height / 2);
+
+  //   let newPosition = {
+  //     x: event.pageX - colorPickerOffset.x,
+  //     y: event.pageY - colorPickerOffset.y
+  //   };
+
+  //   console.log("newPosition", newPosition);
+
+  //   setDragHandlePosition(newPosition);
+
+  //   setIsDragging(false);
+
+
+  // };
+
+
   const handleDragStart = (event) => {
-
-    event.dataTransfer.setDragImage(dragItemRef.current, event.target.getBoundingClientRect().width / 2, event.target.getBoundingClientRect().height / 2);
-
-    setIsDragging(true);
-
-  };
-
-
-  const handleDrop = (event) => {
-
-    event.dataTransfer.setDragImage(dragItemRef.current, dragItemRef.current.getBoundingClientRect().width / 2, dragItemRef.current.getBoundingClientRect().height / 2);
-
-    // pageX = saturation 0 - 100
-    // pageY = brightness (not lightness) 0 - 100
-
-    // https://stackoverflow.com/a/31851617
-    // function hsv_to_hsl(h, s, v) {
-    //   // both hsv and hsl values are in [0, 1]
-    //   var l = (2 - s) * v / 2;
-
-    //   if (l != 0) {
-    //     if (l == 1) {
-    //       s = 0;
-    //     } else if (l < 0.5) {
-    //       s = s * v / (l * 2);
-    //     } else {
-    //       s = s * v / (2 - l * 2);
-    //     }
-    //   }
-
-    //   return [h, s, l];
-    // }
-
-    let newPosition = {
-      x: event.pageX - colorPickerOffset.x,
-      y: event.pageY - colorPickerOffset.y
-    };
-
-    console.log("newPosition", newPosition);
-
-    setDragHandlePosition(newPosition);
-
-    setIsDragging(false);
-
-    // dragged.parentNode.removeChild(dragged);
-    // event.target.appendChild(dragged);
-
-  };
-
-
-  const handleClick = (event) => {
 
     if (isDragging === false) {
 
-      // setIsDragging(true);
+      setIsDragging(true);
 
       let newPosition = {
         x: event.pageX - colorPickerOffset.x,
         y: event.pageY - colorPickerOffset.y
       };
-
-      console.log("newPosition", newPosition);
 
       setDragHandlePosition(newPosition);
 
@@ -186,22 +192,46 @@ const ColorPicker = (props) => {
   };
 
 
-  return (
-    <StyledColorPicker $hue={hue} $dragHandlePosition={dragHandlePosition} $isDragging={isDragging} ref={colorPickerRef} onDragOver={(event) => { event.preventDefault(); }} onDrop={(event) => { handleDrop(event); }} onDragLeave={(event) => { handleDrop(event); }} onClick={(event) => { handleClick(event); }}>
-      <div
-        className="color-picker-cursor"
-        draggable="true"
-        style={{
-          // ? use dragItemRef so that 10px isn't hard coded? -- 12/30/2023 JH
-          top: `calc(${dragHandlePosition.y}px - 10px)`,
-          left: `calc(${dragHandlePosition.x}px - 10px)`
-        }}
-        onDragStart={(event) => { handleDragStart(event); }}
-      // onDragEnd={(event) => { handleDragEnd(event); }}
-      // onDrag={(event) => { handleDrag(event); }}
-      ></div>
+  const handleDrag = (event) => {
 
-      <div ref={dragItemRef} className="drag-item-copy" tabIndex="-1"></div>
+    if (isDragging === true) {
+
+      let newPosition = {
+        x: event.pageX - colorPickerOffset.x,
+        y: event.pageY - colorPickerOffset.y
+      };
+
+      setDragHandlePosition(newPosition);
+
+    };
+
+  };
+
+
+  const handleDragEnd = (event) => {
+
+    console.log("event.type", event.type);
+
+    if (isDragging === true) {
+      setIsDragging(false);
+    };
+
+  };
+
+
+  return (
+    <StyledColorPicker
+      $hue={hue}
+      $dragHandlePosition={dragHandlePosition}
+      ref={colorPickerRef}
+      onMouseMove={(event) => { handleDrag(event); }}
+      onMouseLeave={(event) => { handleDragEnd(event); }}
+      onMouseDown={(event) => { handleDragStart(event); }}
+      onMouseUp={(event) => { handleDragEnd(event); }}
+    >
+
+      <div className="color-picker-cursor"></div>
+
     </StyledColorPicker>
   );
 };
